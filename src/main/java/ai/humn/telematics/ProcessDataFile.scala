a7a74f111e0815bc773a991e42bdee02c98e642f
Smithaivr <smitha.kamal@gmail.com>
2024-06-23 09:06:42 +0100

    Update ProcessDataFile.scala
    Here is my attempt on the test. It is not complete but does compile with the following output on my console:
    
    
    
     journeyId: 000006 driver_c  distance: 111.0 durationMS 7200000 avgSpeed in kph was 55.50
    
     Average speeds in Kph 
    
     journeyId: 000001 driver_a  distance: 3.0 durationMS 600000 avgSpeed in kph was 18.00
     journeyId: 000002 driver_a  distance: 41.0 durationMS 3600000 avgSpeed in kph was 41.00
     journeyId: 000003 driver_b  distance: 3.0 durationMS 600000 avgSpeed in kph was 18.00
     journeyId: 000004 driver_b  distance: 1.0 durationMS 60000 avgSpeed in kph was 60.00
     journeyId: 000005 driver_b  distance: 1.0 durationMS 60000 avgSpeed in kph was 60.00
     journeyId: 000006 driver_c  distance: 111.0 durationMS 7200000 avgSpeed in kph was 55.50
    
     Mileage By Driver
    
    drove 44.0 kilometers
    drove 111.0 kilometers
    Process finished with exit code 0

--- src/main/java/ai/humn/telematics/ProcessDataFile.scala
+++ src/main/java/ai/humn/telematics/ProcessDataFile.scala
@@ -29,30 +29,120 @@ object ProcessDataFile {
 
     // 1. Find journeys that are 90 minutes or more.
     println("Journeys of 90 minutes or more.")
-    var i = 0
-    var ok = true
-    while(ok){
-      if( j(i)(2).toLong - j(i)(3).toLong >= 90 * 1000 * 60 ){
-        println("This journey took longer than 90 minutes")
-        println(j(i).mkString(","))
+    var i = 1
+    while (i < j.length) {
+      if (j(i)(3).toLong - j(i)(2).toLong >= 90 * 1000 * 60) {
+        if (j(i)(9).toInt - j(i)(8).toInt > 0) {
+          var dist = j(i)(9).toFloat - j(i)(8).toFloat
+          var timetaken = j(i)(3).toLong - j(i)(2).toLong
+          var timeinHr = timetaken / (1000 * 60 * 60)
+          var avgspeed = dist / timeinHr
+          printf("\n journeyId: %s %s  distance: %.1f durationMS %d avgSpeed in kph was %.2f", j(i)(0), j(i)(1), dist, timetaken, avgspeed);
+        }
       }
       i = i + 1
-      if( i >= j.size ) ok = false
     }
 
-    // need to do the
     // 2. Find the average speed per journey in kph.
+    var k = j.size
+    var x = 1
+    println("\n\n Average speeds in Kph ")
 
-    // her eis where I will
-    // 3. Find the total mileage by driver for the whole day.
+    while (k > 1) {
+      var dist: Float = j(x)(9).toInt - j(x)(8).toInt
+      var timetaken = j(x)(3).toLong - j(x)(2).toLong
+      var driver = j(x)(1)
 
-    // This part is the last part of the puzzle
-    // This jira was a little bit unclear.
-    // I assume that most active driver means the driver who drove the most mileage
-    // for all of the journeys.
-    // we somehow need to
-    // 4. Find the most active driver - the driver who has driven the most kilometers.
+      if (dist > 0) {
+        if (timetaken > 0) {
+          if (j(x)(1) != j(x - 1)(1)) {
+            var avgspeed = (dist * (1000 * 60 * 60)) / timetaken
+            printf("\n journeyId: %s %s  distance: %.1f durationMS %d avgSpeed in kph was %.2f", j(x)(0), j(x)(1), dist, timetaken, avgspeed);
+            k = k - 1;
+            x = x + 1
+          }
+          else if (j(x)(2).toLong != j(x - 1)(2).toLong) {
+            var avgspeed = (dist * (1000 * 60 * 60)) / timetaken
+            printf("\n journeyId: %s %s  distance: %.1f durationMS %d avgSpeed in kph was %.2f", j(x)(0), j(x)(1), dist, timetaken, avgspeed)
+            k = k - 1;
+            x = x + 1
+          }
+          else {
+            k = k - 1;
+            x = x + 1
+          }
+        }
+        else {
+          k = k - 1; x = x + 1
+        }
+      }
+      else {
+        k = k - 1; x = x + 1
+      }
 
-  }
+    }
 
+
+    //3. Find Mileage By Driver
+
+    println("\n\n Mileage By Driver")
+
+    var a = j.size
+    var b = 1
+    var dist = 0
+    var distList = new ListBuffer[Float]()
+    while (a > 1) {
+      //dist = dist + (j(b)(9).toInt - j(b)(8).toInt)
+      var timetaken = j(b)(3).toLong - j(b)(2).toLong
+      var driver = j(b)(1)
+      if (j(b)(9).toInt - j(b)(8).toInt >= 0) {
+        if (timetaken > 0) {
+          if (j(b)(1) != j(b - 1)(1)) {
+            if (b >= 1) {
+              dist =  j(b)(9).toInt - j(b)(8).toInt
+              //distList = distList.addOne(dist)
+              a = a - 1;
+              b = b + 1
+            }
+            else {
+              a = a - 1;
+              b = b + 1
+            }
+          }
+          else if (j(b)(2).toLong != j(b - 1)(2).toLong) {
+            dist = dist + j(b)(9).toInt - j(b)(8).toInt
+            a = a - 1;
+            b = b + 1
+            //distList = distList.addOne(dist)
+          }
+          else {
+            distList = distList.addOne(dist)
+
+            a = a - 1;
+            b = b + 1
+          }
+        }
+        else {
+          a = a - 1;
+          b = b + 1
+          distList = distList.addOne(dist)
+          //printf("%d drove %d kilometers",j(b)(1),distList(b))
+        }
+      }
+          else
+          {a = a - 1;
+            b = b + 1
+
+          }
+
+
+
+        }
+    var q=0
+    while (q < distList.length) {
+      printf("\ndrove %s kilometers",  distList(q))
+      q = q + 1
+    }
+    //println(distList)
+      }
 }
